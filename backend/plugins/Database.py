0b3a3da4b5baf1c3e560f14633b2786d8d3b7f5b
ksw-1024 <orimun.mohumohu@gmail.com>
2024-07-12 18:27:57 +0900

    SQLiteを使うための関数を定義中(工事中)

--- backend/plugins/Database.py
+++ backend/plugins/Database.py
@@ -9,26 +9,37 @@ def randomname(n):
    randlst = [random.choice(string.ascii_letters + string.digits) for i in range(n)]
    return ''.join(randlst)
 
-def data_push():
+def data_push(filename, sepatate_filename, word):
     conn = sqlite3.connect(os.path.join(currentDir, "SQLite3","AUDIO_DATA.db"))
     
     cur = conn.cursor()  # カーソルを作成
-    cur.execute('DROP TABLE audio')
     
-    cur.execute('CREATE TABLE IF NOT EXISTS audio(id INTEGER PRIMARY KEY AUTOINCREMENT,time TIMESTAMP, sepatate_filename STRING, word STRING, filename STRING)')  # tableを作成する指示
-    #cur.execute('INSERT INTO audio(time, filename, word) VALUES(CURRENT_TIMESTAMP, "filename.wav", "てにほは")')
+    cur.execute('CREATE TABLE IF NOT EXISTS audio(id INTEGER PRIMARY KEY AUTOINCREMENT,time TIMESTAMP, filename STRING, separate_filename STRING, word STRING)')  # tableを作成する指示
+    cur.execute('INSERT INTO audio(time, filename, separate_filename, word) VALUES(CURRENT_TIMESTAMP, "{}", "{}", "{}")'.format(filename, sepatate_filename, word))
     
-    conn.commit()  # commit()した時点でDBファイルが更新されます
-    
-    cur.execute("SELECT name from sqlite_master where type='table';")
-    print('table一覧: ', cur.fetchall())
-    
-    #cur.execute('SELECT * FROM audio WHERE filename="filename.wav"')
-    #word_data = cur.fetchall()
-    
-    #print(word_data)
+    conn.commit()  # commitした時点でDBファイルが更新されます
     
     conn.close()
     
+def delete_table():
+    conn = sqlite3.connect(os.path.join(currentDir, "SQLite3","AUDIO_DATA.db"))
+    
+    cur = conn.cursor()  # カーソルを作成
+    cur.execute('CREATE TABLE IF NOT EXISTS audio(id INTEGER PRIMARY KEY AUTOINCREMENT,time TIMESTAMP, filename STRING, separate_filename STRING, word STRING)')
+    cur.execute('DROP TABLE audio')
+    
+    conn.commit()
+    conn.close()
+    
+def get_info(filename):
+    conn = sqlite3.connect(os.path.join(currentDir, "SQLite3","AUDIO_DATA.db"))
+    cur = conn.cursor()
+    
+    cur.execute('SELECT * FROM audio WHERE filename="{}"'.format(filename))
+    word_data = cur.fetchall()
+    
+    conn.close()
+    return word_data
+    
 if __name__ == '__main__':
-    data_push()
+    delete_table()
